8dfcf6e7bb2f3311f0ca6294fd3b5654c01dbe2c
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2024-01-22 02:33:54 -0600

    Added Python scripts to deploy and test the mod.

---
+++ scripts/deploy.py
@@ -0,0 +1,20 @@
+﻿import shutil
+import dataclasses
+import sys
+import logging
+
+if not sys.argv:
+    print("A root directory must be passed.")
+    sys.exit(1)
+
+directory: str = sys.argv[1]
+
+print("Copying files to " + directory)
+shutil.rmtree(directory, ignore_errors=True)
+shutil.copytree("About/", directory + "/About/", dirs_exist_ok=True)
+shutil.copytree("Common/", directory + "/Common/", dirs_exist_ok=True)
+shutil.copyfile("LICENSE", directory + "/LICENSE")
+shutil.copyfile("README.md", directory + "/README.md")
+shutil.copyfile("LoadFolders.xml", directory + "/LoadFolders.xml")
+shutil.copyfile("Corpus.xml", directory + "/Corpus.xml")
+shutil.copytree("Releases/", directory + "/Releases/", dirs_exist_ok=True)
---
+++ scripts/test/log.py
@@ -0,0 +1,44 @@
+import enum
+import logging
+
+
+class LogMessageType(enum.StrEnum):
+    UNKNOWN = "UNKNOWN"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    DEBUG = "DEBUG"
+    FATAL = "FATAL"
+    CRITICAL = "CRITICAL"
+
+
+class LogMessageTyper:
+    def __init__(self):
+        self._identifiers: dict[str, LogMessageType] = {
+            "INFO": LogMessageType.INFO,
+            "WARN": LogMessageType.WARNING,
+            "DEBUG": LogMessageType.DEBUG,
+            "ERR": LogMessageType.ERROR,
+            "CRIT": LogMessageType.CRITICAL,
+            "FATAL": LogMessageType.FATAL
+        }
+
+    def type(self, message: str) -> LogMessageType:
+        for key, value in self._identifiers.items():
+            token_length: int = len(key)
+
+            if len(message) < token_length:
+                continue
+
+            has_prefix = message[0] in ("(", "[", "<")
+            level = message[1:token_length + 1] if has_prefix else message[:token_length]
+            if level.upper() != key.upper():
+                continue
+
+            return value
+
+        return LogMessageType.UNKNOWN
+
+
+def register_text_type():
+    logging.addLevelName(15, "TEXT")
