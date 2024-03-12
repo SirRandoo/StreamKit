8aa3e2714cad5b7599a2e9189d1572e72844542a
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2024-03-11 21:02:55 -0500

    Removed deprecated code.

--- src/StreamKit.Server.WebApp/ClientApp/src/app/counter/counter.component.spec.ts
+++ src/StreamKit.Server.WebApp/ClientApp/src/app/counter/counter.component.spec.ts
@@ -1,11 +1,11 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 
 import { CounterComponent } from "./counter.component";
 
 describe("CounterComponent", () => {
   let fixture: ComponentFixture<CounterComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [CounterComponent]
     })
@@ -17,7 +17,7 @@ describe("CounterComponent", () => {
     fixture.detectChanges();
   });
 
-  it("should display a title", async(() => {
+  it("should display a title", waitForAsync(() => {
     const titleText = fixture.nativeElement.querySelector("h1").textContent;
     expect(titleText).toEqual("Counter");
   }));
