57669ce26313d8a5e66270df964144e7c7d19f70
SirRandoo <8146689+SirRandoo@users.noreply.github.com>
2024-09-04 17:50:30 -0500

    Updated @angular/cli (17.3.6 -> 18.2.3).

--- src/StreamKit.Server.WebApp/ClientApp/src/app/app.module.ts
+++ src/StreamKit.Server.WebApp/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { APP_ID, NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
@@ -10,29 +10,23 @@ import { CounterComponent } from "./counter/counter.component";
 import { FetchDataComponent } from "./fetch-data/fetch-data.component";
 import { NgClass, NgForOf, NgIf } from "@angular/common";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
-  ],
-  imports: [
-    HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot([
-      { path: "", component: HomeComponent, pathMatch: "full" },
-      { path: "counter", component: CounterComponent },
-      { path: "fetch-data", component: FetchDataComponent },
-    ]),
-    NgClass,
-    NgIf,
-    NgForOf
-  ],
-  providers: [
-    { provide: APP_ID, useValue: "ng-cli-universal" }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        CounterComponent,
+        FetchDataComponent
+    ],
+    bootstrap: [AppComponent], imports: [FormsModule,
+        RouterModule.forRoot([
+            { path: "", component: HomeComponent, pathMatch: "full" },
+            { path: "counter", component: CounterComponent },
+            { path: "fetch-data", component: FetchDataComponent },
+        ]),
+        NgClass,
+        NgIf,
+        NgForOf], providers: [
+        { provide: APP_ID, useValue: "ng-cli-universal" },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule {}
